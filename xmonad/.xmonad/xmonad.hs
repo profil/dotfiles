2ac4803b60bf9b08f3947b33907f4607ebb66f0a
profil <profil@errno.se>
2018-03-19 01:39:12 +0100

    Update xmonad config

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -1,12 +1,49 @@
-import XMonad
-import XMonad.Config.Desktop
-import XMonad.Layout.Spacing
+import qualified Data.Map as M
+import qualified XMonad.StackSet as W
 
-main = xmonad desktopConfig
-    { terminal = "alacritty"
-    , modMask = mod4Mask
-    , borderWidth = 5
-    , normalBorderColor = "#282a36"
-    , focusedBorderColor = "#bd93f9"
-    , layoutHook = spacing 10 $ Tall 1 (3/100) (3/5) ||| Full
-    }
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.ManageHelpers
+import XMonad.Layout.Spacing
+import XMonad.Layout.PerWorkspace
+import XMonad.Layout.NoBorders
+import XMonad.Layout.SimpleFloat
+import XMonad.Util.Scratchpad
+
+import XMonad.Prompt
+import XMonad.Prompt.Theme
+
+import Graphics.X11.ExtraTypes.XF86
+
+main = xmonad =<< xmobar myConfig
+
+myConfig = ewmh defaultConfig
+  {terminal = "alacritty",
+   modMask = mod4Mask,
+   borderWidth = 2,
+   normalBorderColor = "#343C48",
+   focusedBorderColor = "#6986a0",
+   workspaces = myWorkspaces,
+   layoutHook = myLayouts,
+   keys = keys defaultConfig <+> myKeys,
+   manageHook = manageHook defaultConfig <+> myManageHook,
+   handleEventHook = handleEventHook defaultConfig <+> fullscreenEventHook}
+
+myWorkspaces = ["web", "steam", "other"]
+
+myLayouts = onWorkspace "steam" simpleFloat $
+            smartSpacing 10 $ Tall 1 (3/100) (3/5) ||| noBorders Full
+
+myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList
+  [((modMask, xK_section), scratchpadSpawnActionCustom "alacritty -d 0 0 -t scratchpad"),
+   ((0, xF86XK_AudioRaiseVolume), spawn "pactl set-sink-volume @DEFAULT_SINK@ +5%"),
+   ((0, xF86XK_AudioLowerVolume), spawn "pactl set-sink-volume @DEFAULT_SINK@ -5%"),
+   ((0, xF86XK_AudioMute), spawn "pactl set-sink-mute @DEFAULT_SINK@ toggle")]
+
+myManageHook = composeAll
+  [className =? "hl2_linux" --> doShift "steam",
+   className =? "Steam" --> doShift "steam",
+   className =? "dota2" --> doIgnore,
+   className =? "Pavucontrol" --> doCenterFloat,
+   scratchpadManageHook (W.RationalRect 0.2 0.2 0.6 0.6)]
