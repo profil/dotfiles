9a124993d9f3d8017810adde6e8e53466df86435
Daniel Skoog <profil@errno.se>
2025-02-19 08:51:25 +0000

    Update neovim indent blankline config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -114,9 +114,10 @@ require("lazy").setup({
     "lukas-reineke/indent-blankline.nvim",
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help indent_blankline.txt`
+    main = "ibl",
     opts = {
       --char = "┊",
-      show_trailing_blankline_indent = false,
+      --show_trailing_blankline_indent = false,
     },
   },
 
